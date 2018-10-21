a7244629d6a67ae0b7bb78bfae9a1c7fdf2bc08f
roge4ka <roge.14.10.1998@gmail.com>
2018-10-21 18:19:28 +0300

    Code Review by Egor Chernetsov

--- app/src/main/java/com/hyperdev/androidme/activities/MainActivity.java
+++ app/src/main/java/com/hyperdev/androidme/activities/MainActivity.java
@@ -2,14 +2,18 @@ package com.hyperdev.androidme.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.hyperdev.androidme.R;
-
+/*
+This is the mainActivity
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Activity created", Toast.LENGTH_SHORT).show();
     }
 }
